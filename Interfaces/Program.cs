3a5d68fba495f1251e0c06d8141c0d8759ea8ef0
Suat SATILMIŞ <suatstlm41@gmail.com>
2022-01-30 23:05:03 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CustomerManager customerManager = new CustomerManager();
+            //customerManager.Add(new OracleCustomerDal());
+
+            ICustomerDal[] customerDals = new ICustomerDal[]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Delete();
+            }
+
+
+        }
+    }
+}
---
+++ Interfaces/SqlServerCustomerDal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class SqlServerCustomerDal :ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated");
+        }
+    }
+}
